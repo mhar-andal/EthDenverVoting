7c31b591197efea61b13b185943d350465105d5d
Mhar Andal <mhar.andal@gamut.com>
2018-02-17 10:54:28 -0700

    Remove ElectionRgistry from voting

--- dapp/migrations/2_voting.js
+++ dapp/migrations/2_voting.js
@@ -1,7 +1,6 @@
 const deployInfo = require('../helpers/deployInfo');
 const Web3 = require('web3');
 const Voting = artifacts.require('Voting');
-const ElectionRegistry = artifacts.require('ElectionRegistry');
 const asciiToHex = (Web3.utils || {}).asciiToHex || Web3.prototype.fromAscii;
 //                 ^^^ web3 1.x                     ^^^ web3 0.20.X
 
@@ -10,9 +9,4 @@ const candidates = ['Rama', 'Nick', 'Jose'];
 module.exports = async deployer => {
   await deployer.deploy(Voting, candidates.map(asciiToHex));
   return deployInfo(deployer, Voting);
-};
-
-module.exports = async deployer => {
-  await deployer.deploy(ElectionRegistry, 'Denver Governor Election');
-  return deployInfo(deployer, ElectionRegistry);
 };
