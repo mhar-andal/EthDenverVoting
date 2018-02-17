cbe6d14c871b81104c69e0c149a30a628e4c88df
Mhar Andal <mhar.andal@gamut.com>
2018-02-17 00:12:09 -0700

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+/* global process */
+//
+// import React from 'react';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+
+const Main = props => (
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <AppBar title="My AppBar" />
+    <App {...props} />
+  </MuiThemeProvider>
+);
+
+// export default Main;
+
+const env = {
+  network: process.env.REACT_APP_ETH_NETWORK || 'development', // Default to Ganache CLI
+};
+ReactDOM.render(<Main {...env} />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
