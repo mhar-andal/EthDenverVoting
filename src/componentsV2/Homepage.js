77c70b1ad79a11634fa92158149b4e63765a6fe0
Josh Cheek <josh.cheek@gmail.com>
2018-02-18 14:38:23 -0600

    Center the flag

--- src/componentsV2/Homepage.js
+++ src/componentsV2/Homepage.js
@@ -5,7 +5,7 @@ import "./Homepage.css";
 class Homepage extends React.Component {
   renderFlag = () => {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" width="540" height="300">
+      <svg xmlns="http://www.w3.org/2000/svg" width="600" height="300">
         <path d="M0 0H600V360H0" fill="#002868" />
         <path d="M0 360V221L120 88 300 288 480 88 600 221v139" fill="#fff" />
         <path
